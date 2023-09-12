00ffd06375a75d8bb31f67e76d186818d11cf7f7
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-09-12 20:25:14 +0530

    camera lecture progress

--- 01_Basics/05_Camera/07-cameras/src/script.js
+++ 01_Basics/05_Camera/07-cameras/src/script.js
@@ -1,6 +1,19 @@
 import "./style.css";
 import * as THREE from "three";
 
+/*
+Cursor
+*/
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (e) => {
+  // console.log(e.clientX);
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
+
 /**
  * Base
  */
@@ -24,6 +37,15 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
+const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
@@ -52,8 +74,12 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
 
+  //Update Camera
+  camera.position.x = cursor.x * 3;
+  camera.position.y = cursor.y * 3;
+  camera.lookAt(mesh.position);
   // Render
   renderer.render(scene, camera);
 
